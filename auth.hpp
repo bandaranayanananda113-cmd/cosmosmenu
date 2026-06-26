821545475bb618c77c394a79483de558d9862593
bandaranayanananda113-cmd <bandaranayanananda113@gmail.com>
2026-06-27 01:54:49 +0530

    Update auth.hpp

--- auth.hpp
+++ auth.hpp
@@ -1,3 +1,25 @@
 #pragma once
+#include <string>
 
+namespace KeyAuth {
+    class api {
+    public:
+        std::string name;
+        std::string ownerid;
+        std::string secret;
+        std::string version;
+        std::string url;
 
+        api(std::string name, std::string ownerid, std::string secret, std::string version, std::string url) {
+            this->name = name;
+            this->ownerid = ownerid;
+            this->secret = secret;
+            this->version = version;
+            this->url = url;
+        }
+        
+        void init() {
+            // Initialization code
+        }
+    };
+}
