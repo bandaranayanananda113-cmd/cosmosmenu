1bb8d914717550e40961645ec895a907336056b5
bandaranayanananda113-cmd <bandaranayanananda113@gmail.com>
2026-06-27 01:55:57 +0530

    Update skStr.h

--- skStr.h
+++ skStr.h
@@ -8,12 +8,12 @@
 #endif
 
 namespace skc {
-    template <std::size_t N, char K, typename T>
+    template <std::size_t N, char K>
     class skCrypter {
     public:
         std::array<char, N> _storage;
 
-        FORCEINLINE constexpr skCrypter(const T* data) {
+        FORCEINLINE constexpr skCrypter(const char* data) {
             for (std::size_t i = 0; i < N; ++i) {
                 _storage[i] = data[i] ^ K;
             }
@@ -31,6 +31,6 @@ namespace skc {
 #define skCrypt(str) []() { \
     constexpr char key = __TIME__[4] ^ __TIME__[7]; \
     constexpr std::size_t len = sizeof(str); \
-    static auto crypted = skc::skCrypter<len, key, char>(str); \
+    static auto crypted = skc::skCrypter<len, key>(str); \
     return crypted.get(); \
 }()
